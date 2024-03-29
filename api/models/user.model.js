177102d0d0005c11f6c66aeb77863af4ce460d67
Avalon Lucero Munoz <102561168+AvalonLucero@users.noreply.github.com>
2024-03-29 14:15:09 -0500

    Create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,26 @@
+import { timeStamp } from "console";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: "string",
+      required: true,
+      unique: true, //same username or not as existing
+    },
+    email: {
+      type: "string",
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: "string",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
