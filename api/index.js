92f70e6784e907e5e4017e3514f2610f06204bb4
Avalon Lucero Munoz <102561168+AvalonLucero@users.noreply.github.com>
2024-03-29 14:53:05 -0500

    Fix database connection

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv"; //Be able to use env for security uploading on GitHub
-dotenv.config;
+dotenv.config(); //Initialize it
 
 //Connect to the database
 mongoose
@@ -11,8 +11,8 @@ mongoose
   .then(() => {
     console.log("Connected to MongoDb");
   })
-  .catch((err) => {
-    console.log(err``);
+  .catch((error) => {
+    console.log(error);
   });
 
 const app = express();
@@ -22,4 +22,9 @@ app.listen(3000, () => {
   //Terminal run: node api/index.js -> installed nodemon and added script
   //Terminal run: npm run dev (server restarts automatically)
   console.log("Server is running on port 3000");
+});
+
+//Api Route to overslash
+app.get("/test", (req, res) => {
+  res.send("Hello, world!");
 });
