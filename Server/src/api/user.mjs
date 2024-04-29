9906c58e7dff7519053dd4854e189a8abb254091
Avalon Lucero Munoz <102561168+AvalonLucero@users.noreply.github.com>
2024-04-28 21:48:48 -0500

    Routes

---
+++ Server/src/api/user.mjs
@@ -0,0 +1,50 @@
+import express from "express";
+import authenticate from "../middleware/authenticate.mjs";
+import firebaseAdmin from "../services/firebase.mjs";
+
+const router = express.Router();
+
+//Get user router
+router.get("/", authenticate, async (req, res) => {
+  res.status(200).json(req.user);
+});
+
+//Sign up route
+router.post("/", async (req, res) => {
+  const { email, name, password } = req.body;
+
+  if (!email || !name || !password) {
+    return res.status(400).json({
+      error:
+        "Invalid request body. Must contain email, password, and name for user.",
+    });
+  }
+
+  try {
+    const newFirebaseUser = await firebaseAdmin.auth.createUser({
+      email,
+      password,
+    });
+
+    if (newFirebaseUser) {
+      const userCollection = req.app.locals.db.collection("user");
+      await userCollection.insertOne({
+        email,
+        name,
+        firebaseId: newFirebaseUser.uid,
+      });
+    }
+    return res
+      .status(200)
+      .json({ success: "Account created successfully. Please sign in." });
+  } catch (err) {
+    if (err.code === "auth/email-already-exists") {
+      return res
+        .status(400)
+        .json({ error: "User account already exists at email address." });
+    }
+    return res.status(500).json({ error: "Server error. Please try again" });
+  }
+});
+
+export default router;
