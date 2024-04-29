a132d6a0466a8c74c4f8b5b27ba7e12844343d06
Avalon Lucero Munoz <102561168+AvalonLucero@users.noreply.github.com>
2024-04-29 17:29:17 -0500

    Showing react pages now

---
+++ real-estate/src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <StoreProvider store={store}>
+      <App />
+    </StoreProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
