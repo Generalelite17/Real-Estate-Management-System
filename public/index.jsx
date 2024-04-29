934049f6d2b2e2daed97b78e053d1c5cf9bca4e6
Avalon Lucero Munoz <102561168+AvalonLucero@users.noreply.github.com>
2024-04-29 00:34:42 -0500

    Fixing

---
+++ public/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Container() {
+  return (
+    <div className="container">
+      <h1>Hello World!</h1>
+      <p>This is a sample paragraph.</p>
+    </div>
+  );
+}
+
+export default Container;
