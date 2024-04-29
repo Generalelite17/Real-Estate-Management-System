dc633f58a8edea20e0c6756d78b6723a2fad88e2
Avalon Lucero Munoz <102561168+AvalonLucero@users.noreply.github.com>
2024-04-28 21:43:48 -0500

    Firebase configuration

--- Server/src/server.mjs
+++ Server/src/server.mjs
@@ -1,18 +1,24 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mongoose = require("mongoose");
 
 const config = require("./config/index.mjs");
 const db = require("./config/db.mjs");
-const userRouter = require("./api/user.mjs");
+//const userRouter = require("./api/user.mjs");
 
 db(config.MONGO_URI, app);
 
 app.use(cors({ origin: true }));
 app.use(express.json());
-app.use("/api/user", userRouter);
 
-app.listen(config.PORT, () =>
-  console.log(`App listening on PORT ${config.PORT}`)
-);
+//app.use("/api/user", userRouter);
+app.get("/", (req, res) => {
+  res.render("login");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+const port = 8000;
+app.listen(port, () => console.log(`App listening on PORT ${port}`));
