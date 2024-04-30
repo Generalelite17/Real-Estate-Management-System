cfc4f60431275cc9ec69a1b4358200b10b318a0a
Generalelite17 <emmanuelctx17@gmail.com>
2024-04-30 02:22:04 -0500

    Completed Front End Part

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./GetStarted.css";
+
+const GetStarted = () => {
+  return (
+    <div className="padding innerWidth g-container">
+      <div className="flexColCenter inner-container">
+        <span className="primaryText">Get Started with Us</span>
+        <span className="secondayText">
+          Subscribe and find super attractive prive quotes
+          <br />
+          Find your residence soon
+        </span>
+        <button className="button">
+            <a href="mailto:emmanuelctx17@gmail.com">Get Started</a>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default GetStarted;
