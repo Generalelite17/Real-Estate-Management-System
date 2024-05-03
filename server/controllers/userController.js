e9e1c47d55820d4b872e42b7e6e7c35129a45122
Avalon Lucero Munoz <102561168+AvalonLucero@users.noreply.github.com>
2024-05-03 00:03:56 +0000

    Attempting to have connection to MongoDB

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -9,14 +9,14 @@ export const createUser = asyncHandler(async (req, res) => {
 
   console.log(email);
   
-  /**const userExists = await prisma.user.findUnique({ where: { email: email } });
+  const userExists = await prisma.user.findUnique({ where: { email: email } });
   if (!userExists) {
     const user = await prisma.user.create({ data: req.body });
     res.send({
       message: "User registered successfully",
       user: user,
     });
-  } else res.status(201).send({ message: "User already registered" });**/
+  } else res.status(201).send({ message: "User already registered" });
 });
 
 // function to book a visit to resd
