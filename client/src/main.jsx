652891d3e722f2672083e77483b9c2f221b14ec8
Avalon Lucero Munoz <102561168+AvalonLucero@users.noreply.github.com>
2024-05-06 22:17:04 -0500

    Updates

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,14 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-wcfbhpdarp1ktbya.us.auth0.com"
+      clientId="TuZgVFUoYaN9GhBu2Azkf5lJ4nfcq5a9"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
