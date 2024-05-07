61f2d77e9131e3ae15656d919ecc466eaa7b5361
Avalon Lucero Munoz <102561168+AvalonLucero@users.noreply.github.com>
2024-05-07 16:21:49 -0500

    Working on fixes

---
+++ client/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCeBqYoZFKdbFeERN4TAovz413CBXX9SsM",
+  authDomain: "realestatemanagementsyst-6389.firebaseapp.com",
+  projectId: "realestatemanagementsyst-6389",
+  storageBucket: "realestatemanagementsyst-6389.appspot.com",
+  messagingSenderId: "508569579924",
+  appId: "1:508569579924:web:a773428577f027005d5f81",
+  measurementId: "G-WF2VET9CDD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ client/src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain="dev-wcfbhpdarp1ktbya.us.auth0.com"
+      clientId="TuZgVFUoYaN9GhBu2Azkf5lJ4nfcq5a9"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
